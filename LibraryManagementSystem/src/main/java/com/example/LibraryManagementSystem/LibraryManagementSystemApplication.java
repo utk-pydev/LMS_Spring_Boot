89cc3c6d6b0a4261791a1a1d89fb5dc5c92153d9
utk-pydev <utkarshvashisth94@gmail.com>
2023-10-02 22:03:27 +0530

    Library Management System Begins

---
+++ LibraryManagementSystem/src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.LibraryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+
+}
